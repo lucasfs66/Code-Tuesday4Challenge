bf23d2c558a228f53b73c098cea10a73d9592a7b
Lucas <lucasfreiresabino@gmail.com>
2022-11-01 09:51:23 -0600

    the begging

---
+++ index.js
@@ -0,0 +1,13 @@
+const perfectSquare = (num) => {
+    if(Number.isInteger(Math.sqrt(num))){
+        
+    }
+}
+
+
+
+perfectSquare(9) // should return 16 (3x3=9, 4x4=16)
+
+perfectSquare(289) // should return 324 (17x17=289 18x18=324)
+
+perfectSquare(3000) // should return -1 (sq root of 3000 is 54.77)
