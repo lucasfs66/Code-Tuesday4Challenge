8cf736e229636efe6fb03c4cdafcf372a4aa341d
Lucas <lucasfreiresabino@gmail.com>
2022-11-01 09:57:27 -0600

    finish the first challenge

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const perfectSquare = (num) => {
     if(Number.isInteger(Math.sqrt(num))){
-        
+        let number = Math.sqrt(num) + 1
+        return Math.pow(num, 2)
     }
+
+    return -1
 }
 
 
 
-perfectSquare(9) // should return 16 (3x3=9, 4x4=16)
 
-perfectSquare(289) // should return 324 (17x17=289 18x18=324)
 
-perfectSquare(3000) // should return -1 (sq root of 3000 is 54.77)
+console.log(perfectSquare(9)) // should return 16 (3x3=9, 4x4=16)
+
+console.log(perfectSquare(289)) // should return 324 (17x17=289 18x18=324)
+
+console.log(perfectSquare(3000)) // should return -1 (sq root of 3000 is 54.77)
